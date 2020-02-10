4ea9a753d568d01e523ddde0d387c4e723acf57c
Manos Koumentakis <m.koumentakis@itconcept.gr>
2020-02-10 17:59:39 +0200

    updated widget view

--- src/views/widget.php
+++ src/views/widget.php
@@ -14,25 +14,26 @@ Yii::createObject(['class' => $assetBundle])->register($this);
 
 // Open the authorization dilalog in popup window.
 if ($popup) {
-	$options = [];
-	foreach ($services as $name => $service) {
-		$options[$service->id] = $service->jsArguments;
-	}
-	$this->registerJs('$("#' . $id . '").eauth(' . json_encode($options) . ');');
+    $options = [];
+    foreach ($services as $name => $service) {
+        $options[$service->id] = $service->jsArguments;
+    }
+    $this->registerJs('$("#' . $id . '").eauth(' . json_encode($options) . ');');
 }
 
 ?>
 <div class="eauth" id="<?php echo $id; ?>">
-	<ul class="eauth-list">
-		<?php
-		foreach ($services as $name => $service) {
-			echo '<li class="eauth-service eauth-service-id-' . $service->id . '">';
-			echo Html::a($service->title, [$action, 'service' => $name], [
-				'class' => 'eauth-service-link',
-				'data-eauth-service' => $service->id,
-			]);
-			echo '</li>';
-		}
-		?>
-	</ul>
+    <ul class="eauth-list">
+        <?php
+        foreach ($services as $name => $service) {
+            $icname = $name == 'google_oauth' ? 'google-plus' : $name;
+            echo '<li class="eauth-service eauth-service-id-' . $service->id . '">';
+            echo Html::a('<i class="fa fa-'.$icname.'"></i>'.$service->title, [$action, 'service' => $name], [
+                'class' => 'eauth-service-link',
+                'data-eauth-service' => $service->id,
+            ]);
+            echo '</li>';
+        }
+        ?>
+    </ul>
 </div>
